796e3ab687256fb0cfc7161776ceeb34c63a43db
jakehong0521 <jakehong0521@berkeley.edu>
2018-08-03 22:20:16 -0400

    setup 2

--- las/views.py
+++ las/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.core import serializers
 from django.http import HttpResponse
 from django.core.serializers import serialize
-from life.models import User, Item
+from las.models import *
 import json
-from life.models import *
 
 def index(request):
   return render(request, 'las/index.html')
